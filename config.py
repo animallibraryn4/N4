684d1be4d39e2a9d442794da160f6989e2d11c82
animallibraryn4 <nikhil7858978052@gmail.com>
2025-02-24 11:27:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_ID = 123456  # Your Telegram API ID
-API_HASH = "your_api_hash"  # Your Telegram API Hash
-BOT_TOKEN = "your_bot_token"  # Your Bot Token from @BotFather
-MONGO_URI = "your_mongo_uri"  # MongoDB connection string
-ADMINS = [123456789]  # List of Admin IDs
+API_ID = "22299340"# Your Telegram API ID
+API_HASH = "09b09f3e2ff1306da4a19888f614d937"  # Your Telegram API Hash
+BOT_TOKEN = "7632805578:AAHyFiomSTFhIxt56vHnosOREPg2iMU8_TQ"  # Your Bot Token from @BotFather
+MONGO_URI = "mongodb+srv://ynikhilsingh109020:K4LAeHeLGiL7Q7qB@cluster0.6xszk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # MongoDB connection string
+ADMINS = [5380609667]  # List of Admin IDs
 FORCE_SUB = ["@channel1", "@channel2", "@channel3"]  # Optional forced subscription channels
