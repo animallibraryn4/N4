5afb607589fe79e74cce6bf0a109435716995aa8
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-24 19:35:51 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-API_ID = "22299340"# Your Telegram API ID
-API_HASH = "09b09f3e2ff1306da4a19888f614d937"  # Your Telegram API Hash
-BOT_TOKEN = "7632805578:AAHyFiomSTFhIxt56vHnosOREPg2iMU8_TQ"  # Your Bot Token from @BotFather
-MONGO_URI = ""  # MongoDB connection string
-ADMINS = [5380609667]  # List of Admin IDs
-FORCE_SUB = ["@channel1", "@channel2", "@channel3"]  # Optional forced subscription channels
