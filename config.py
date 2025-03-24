0d7a06b0f20b718591f54ee2f6c1fbe401295d60
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-24 21:27:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,35 +1,10 @@
 import os
-import logging
+from dotenv import load_dotenv
 
-# Set up logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-logger = logging.getLogger(__name__)
+load_dotenv()
 
-class Config:
-    # Bot Token from @BotFather
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7632805578:AAHyFiomSTFhIxt56vHnosOREPg2iMU8_TQ")
-    
-    # Bot Username
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "N4_Links_bot")
-    
-    # Telegram API ID and Hash
-    API_ID = int(os.getenv("API_ID", 22299340))
-    API_HASH = os.getenv("API_HASH", "09b09f3e2ff1306da4a19888f614d937")
-    
-    # Admin IDs
-    ADMINS = [int(admin) for admin in os.getenv('ADMINS', '5380609667').split(',') if admin]
-    
-    # Database URI
-    DB_URI = os.getenv("DB_URI", "mongodb+srv://gerbil77001:lkFEusnWzXwe53NU@cluster0.mkeei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    
-    # Message configurations
-    WELCOME_MESSAGE = "Here is your link! Click below to proceed:"
-    JOIN_BUTTON_TEXT = "Join Channel"
-    LINK_EXPIRE_TIME = 86400  # 24 hours
-    RESTART_COOLDOWN = 300  # 5 minutes
-
-# Single config instance
-config = Config()
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))  # Admin user ID
