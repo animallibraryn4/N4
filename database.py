9bd0184d1ed3e36bdfc8efd40d117c0c4510ba28
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-26 12:42:22 +0530

    Delete database.py

--- database.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URI
-
-mongo_client = AsyncIOMotorClient(MONGO_URI)
-db = mongo_client["YourBotDatabase"]
-force_sub_collection = db["force_subscription"]
