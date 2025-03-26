5f169002cd74bd217a5205f08fb1c6283c7d44a6
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-26 12:40:21 +0530

    Delete plugins/restart.py

--- plugins/restart.py
+++
@@ -1,8 +0,0 @@
-import os
-from pyrogram import Client, filters
-from config import ADMIN_ID
-
-@Client.on_message(filters.command("restart") & filters.user(ADMIN_ID))
-async def restart_bot(bot, message):
-    await message.reply("Bot is restarting...")
-    os.system("python3 bot.py")
