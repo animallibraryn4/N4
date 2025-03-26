600eb721fd43fc3cd95bb848c0b4ec0a58ffab43
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-26 13:29:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Website URL (Anime-World)
-ANIME_WORLD_URL = "https://anime-world.co/anime-list/"
+ANIME_WORLD_URL = "https://anime-world.co/category/anime/"
 
 # User-Agent (Prevents Blocking by Website)
 HEADERS = {
