5158bf378ce87d6d3a2c58906ff9bbe351a991d0
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-26 13:12:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 ANIME_WORLD_URL = "https://anime-world.co/anime-list/"
 
 # User-Agent (वेबसाइट ब्लॉक न करे इसलिए)
-HEADERS = {"User-Agent": "Mozilla/5.0"}
+HEADERS = {"User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Mobile Safari/537.36"}
 
 # Telegram Bot Configuration
 TELEGRAM_BOT_TOKEN = "AAHyFiomSTFhIxt56vHnosOREPg2iMU8_TQ"
