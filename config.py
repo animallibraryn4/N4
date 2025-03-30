90cd29481281f495f1ca06838bd59b3d83d995af
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-30 20:32:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = int(os.getenv("API_ID", 22299340))
     API_HASH = os.getenv("API_HASH", "09b09f3e2ff1306da4a19888f614d937")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7548613937:AAGK2KhZcUnumGQJ-oY5bh8B9ojU4uI8sBQ")
     ADMIN_IDS = [5336360484]  # Your user ID
-    CHANNEL_ID = ""  # Your channel
-    VOTE_CHAT_ID =   # Chat where votes will be counted
+    CHANNEL_ID = "@animestorys_1"  # Your channel
+    VOTE_CHAT_ID = "-1001896877147" # Chat where votes will be counted
