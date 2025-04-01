fc449aa563c4884e21582a3ff02fffe31ef837c0
animallibraryn4 <nikhil7858978052@gmail.com>
2025-04-01 12:21:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,12 +14,12 @@ app = Client(
 )
 
 
-@app.on_message(filters.command("start") & filters.private & filters.user(5336360484))
+@app.on_message(filters.command("start") & filters.private & filters.user(5380609667))
 async def start(client, message: Message):
     await message.reply_text("Working...")
 
 
-@app.on_message(filters.command("get") & filters.private & filters.user(5336360484))
+@app.on_message(filters.command("get") & filters.private & filters.user(5380609667))
 async def newUpload(client: Client, message: Message):
     try:
         proc = await message.reply_text("Getting Archive Urls...")
@@ -83,7 +83,7 @@ async def newUpload(urls, client: Client, message: Message, proc: Message):
         await message.reply_text(str(e))
 
 
-@app.on_message(filters.command("post") & filters.private & filters.user(5336360484))
+@app.on_message(filters.command("post") & filters.private & filters.user(5380609667))
 async def postAnime(client: Client, message: Message):
     try:
         id = message.text.split(" ", 1)[1]
