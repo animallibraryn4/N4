11cff33e661a0667a6c2a6a1e7fb18a5000b58a8
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-16 02:05:05 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,2 +1,7 @@
-#nikhil
+from . import commands
+from . import anime_handler
+from . import torrent_handler
+from . import utils
+from . import web_scraper
 
+__all__ = ['commands', 'anime_handler', 'torrent_handler', 'utils', 'web_scraper']
