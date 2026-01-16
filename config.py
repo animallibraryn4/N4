6bea2b528bf78b6e8771045473537c36dbda5f13
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-16 12:18:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,54 +1,48 @@
-from pydantic_settings import BaseSettings
-from typing import List, Optional
 import os
+from typing import List
 
-class Config(BaseSettings):
+class Config:
     # Bot Information
-    AUTHOR: str = "Yash-Kun"
-    LICENSED_UNDER: str = "GNU GPL V-3.0"
+    AUTHOR = "Yash-Kun"
+    LICENSED_UNDER = "GNU GPL V-3.0"
     
-    # Telegram Configuration
-    API_ID: int = 22299340
-    API_HASH: str = "09b09f3e2ff1306da4a19888f614d937"
-    MAIN_BOT_TOKEN: str = "7765824536:AAFxnM5qftx3LWjLjxKsnXeuLf3XMXo950o"
-    CLIENT_BOT_TOKEN: str = "7906213903:AAEvUmwjmwTHC9NtayJ2NLbYsROQdaOeZR0"
+    # Telegram Configuration (Update these in .env or here)
+    API_ID = int(os.getenv("API_ID", "22299340"))
+    API_HASH = os.getenv("API_HASH", "")
+    MAIN_BOT_TOKEN = os.getenv("MAIN_BOT_TOKEN", "")
+    CLIENT_BOT_TOKEN = os.getenv("CLIENT_BOT_TOKEN", "")
     
     # Chat IDs
-    OWNER_IDS: List[int] = [5380609667]
-    MAIN_CHANNEL: int = -1001896877147
-    FILES_CHANNEL: int = -1001896877147
-    PRODUCTION_CHAT: int = -1001925970923
+    OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS", "5380609667").split(",")]
+    MAIN_CHANNEL = int(os.getenv("MAIN_CHANNEL", "-1001896877147"))
+    FILES_CHANNEL = int(os.getenv("FILES_CHANNEL", "-1001896877147"))
+    PRODUCTION_CHAT = int(os.getenv("PRODUCTION_CHAT", "-1001925970923"))
     
     # Database
-    DATABASE_URL: str = "mongodb+srv://mikota4432:jkJDQuZH6o8pxxZe@cluster0.2vngilq.mongodb.net/?retryWrites=true&w=majority"
-    DATABASE_NAME: str = "AAB"
+    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://mikota4432:jkJDQuZH6o8pxxZe@cluster0.2vngilq.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_NAME = "AAB"
     
     # File Paths
-    DOWNLOADS_DIR: str = "./downloads"
-    THUMBNAIL_URL: str = "https://files.catbox.moe/50drro.jpg"
-    LOG_FILE: str = "bot.log"
+    DOWNLOADS_DIR = "./downloads"
+    THUMBNAIL_URL = "https://files.catbox.moe/50drro.jpg"
+    LOG_FILE = "bot.log"
     
     # RSS Feed
-    RSS_URL: str = "https://subsplease.org/rss"
+    RSS_URL = "https://subsplease.org/rss"
     
     # Intervals (in seconds)
-    ANIME_CHECK_INTERVAL: int = 300  # 5 minutes
-    WORKER_CHECK_INTERVAL: int = 300  # 5 minutes
+    ANIME_CHECK_INTERVAL = 300
+    WORKER_CHECK_INTERVAL = 300
     
     # Post Template
-    POST_TEMPLATE: str = """🔸 {0}
+    POST_TEMPLATE = """🔸 {}
 ➖〰️➖〰️➖〰️➖〰️➖〰️
-🔸 Episode - {1}
+🔸 Episode - {}
 〰️➖〰️➖〰️➖〰️➖〰️➖
-🔸 Status - {2}
+🔸 Status - {}
 ➖〰️➖〰️➖〰️➖〰️➖〰️
 🔸 Quality - Sub
 〰️➖〰️➖〰️➖〰️➖〰️➖
 """
-    
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
 
 config = Config()
-
